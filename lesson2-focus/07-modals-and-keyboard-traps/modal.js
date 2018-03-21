ab7940120d035bbd980de03703d59fabba75d572
davidkvara <davekvaratskhelia@gmail.com>
2018-03-21 23:24:03 +0400

    keyboard trap example

--- lesson2-focus/07-modals-and-keyboard-traps/modal.js
+++ lesson2-focus/07-modals-and-keyboard-traps/modal.js
@@ -2,37 +2,40 @@
 var focusedElementBeforeModal;
 
 // Find the modal and its overlay
-var modal = document.querySelector('.modal');
-var modalOverlay = document.querySelector('.modal-overlay');
+var modal = document.querySelector(".modal");
+var modalOverlay = document.querySelector(".modal-overlay");
 
-var modalToggle = document.querySelector('.modal-toggle');
-modalToggle.addEventListener('click', openModal);
+var modalToggle = document.querySelector(".modal-toggle");
+modalToggle.addEventListener("click", openModal);
 
 function openModal() {
   // Save current focus
   focusedElementBeforeModal = document.activeElement;
 
   // Listen for and trap the keyboard
-  modal.addEventListener('keydown', trapTabKey);
+  modal.addEventListener("keydown", trapTabKey);
 
   // Listen for indicators to close the modal
-  modalOverlay.addEventListener('click', closeModal);
+  modalOverlay.addEventListener("click", closeModal);
   // Sign-Up button
-  var signUpBtn = modal.querySelector('#signup');
-  signUpBtn.addEventListener('click', closeModal);
+  var signUpBtn = modal.querySelector("#signup");
+  signUpBtn.addEventListener("click", closeModal);
 
   // Find all focusable children
-  var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+  var focusableElementsString =
+    'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
   var focusableElements = modal.querySelectorAll(focusableElementsString);
   // Convert NodeList to Array
-  focusableElements = Array.prototype.slice.call(focusableElements);
+  // პრტოტიპის ხმარება არ ვიცი ჯერ და ამიტომ Array.from() ვიყენებ
+  // focusableElements = Array.prototype.slice.call(focusableElements);
+  focusableElements = Array.from(focusableElements);
 
   var firstTabStop = focusableElements[0];
   var lastTabStop = focusableElements[focusableElements.length - 1];
 
   // Show the modal and overlay
-  modal.style.display = 'block';
-  modalOverlay.style.display = 'block';
+  modal.style.display = "block";
+  modalOverlay.style.display = "block";
 
   // Focus first child
   firstTabStop.focus();
@@ -40,27 +43,31 @@ function openModal() {
   function trapTabKey(e) {
     // Check for TAB key press
     if (e.keyCode === 9) {
-
       // SHIFT + TAB
       if (e.shiftKey) {
-
-      // TAB
+        if (document.activeElement === firstTabStop) {
+          e.preventDefault();
+          lastTabStop.focus();
+        }
       } else {
-
+        if (document.activeElement === lastTabStop) {
+          e.preventDefault();
+          firstTabStop.focus();
+        }
       }
     }
 
     // ESCAPE
     if (e.keyCode === 27) {
-
+      closeModal();
     }
   }
 }
 
 function closeModal() {
   // Hide the modal and overlay
-  modal.style.display = 'none';
-  modalOverlay.style.display = 'none';
+  modal.style.display = "none";
+  modalOverlay.style.display = "none";
 
   // Set focus back to element that had it before the modal was opened
   focusedElementBeforeModal.focus();
